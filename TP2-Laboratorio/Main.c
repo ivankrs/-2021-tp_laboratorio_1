1a1a0f7d25443c69c9c6e8457c59502d185333ab
ivan <ivankr6@gmail.com>
2021-05-11 19:20:04 -0300

    11/05.4

--- TP2-Laboratorio/Main.c
+++ TP2-Laboratorio/Main.c
@@ -9,11 +9,39 @@
 
 int main()
 {
+	int opcion;
 
 
+	setbuf(stdout, NULL);
 
+		do
+		{
 
+			printf(">>>- MENU -<<<\n");
+			printf("\n 1. Ingrese primer operador\n");
+			printf(" 2. Ingrese segundo operador\n");
+			printf(" 3. Calcular todas las operaciones\n");
+			printf(" 4. Mostrar todas las operaciones\n");
+			printf(" 5. Salir\n");
+			printf("~Elija una opcion(1-5): ");
 
+			scanf("%d", &opcion);
+
+			switch(opcion)
+			{
+				case 1:
+				break;
+				case 2:
+				break;
+				case 3:
+				break;
+				case 4:
+				break;
+				case 5:
+				break;
+			}
+
+		}while(opcion!=5);
 
 
 	return 0;
